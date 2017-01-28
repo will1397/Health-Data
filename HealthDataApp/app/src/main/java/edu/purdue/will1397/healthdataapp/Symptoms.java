f724d3a6add42f6619514fc09a53632969422653
will1397 <will1397@purdue.edu>
2017-01-28 16:53:03 -0500

    Add files via upload

---
+++ HealthDataApp/app/src/main/java/edu/purdue/will1397/healthdataapp/Symptoms.java
@@ -0,0 +1,13 @@
+package edu.purdue.will1397.healthdataapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Symptoms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptoms);
+    }
+}
