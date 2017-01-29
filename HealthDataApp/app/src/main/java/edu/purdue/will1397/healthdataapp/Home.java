d2e9f54cc9a9c8095e9d185510dac4c71fd17795
will1397 <will1397@purdue.edu>
2017-01-29 10:07:05 -0500

    Increased functionality to buttons

--- HealthDataApp/app/src/main/java/edu/purdue/will1397/healthdataapp/Home.java
+++ HealthDataApp/app/src/main/java/edu/purdue/will1397/healthdataapp/Home.java
@@ -1,7 +1,10 @@
 package edu.purdue.will1397.healthdataapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
@@ -14,6 +17,7 @@ public class Home extends AppCompatActivity {
     LinearLayout linearLayoutHome;
     CheckBox c1;
     ScrollView scrollView;
+    Button symptoms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,25 @@ public class Home extends AppCompatActivity {
         scrollView = (ScrollView) findViewById(R.id.medicalHistory);
         linearLayoutHome = (LinearLayout) findViewById(R.id.linearLayoutHome);
         String checkBoxList[];
-        checkBoxList = new String[] {"a", "b", "c"}; //put the values for the checkboxes here
+        checkBoxList = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}; //put the values for the checkboxes here
 
-        for(int i = 0; i < checkBoxList.length; i++) { //dynamically create checkboxes
+        for (int i = 0; i < checkBoxList.length; i++) { //dynamically create checkboxes
             CheckBox c1 = new CheckBox(this);
             c1.setText(checkBoxList[i]);
             linearLayoutHome.addView(c1);
         }
+
+        final Context context = this;
+        symptoms = (Button) findViewById(R.id.symptoms);
+        symptoms.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Symptoms.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- HealthDataApp/app/src/main/java/edu/purdue/will1397/healthdataapp/Symptoms.java
+++ HealthDataApp/app/src/main/java/edu/purdue/will1397/healthdataapp/Symptoms.java
@@ -1,13 +1,54 @@
 package edu.purdue.will1397.healthdataapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class Symptoms extends AppCompatActivity {
+
+    Button profile;
+    LinearLayout head;
+    LinearLayout chest;
+    LinearLayout uB;
+    LinearLayout lB;
+    LinearLayout gT;
+
+    CheckBox h;
+    CheckBox c;
+    CheckBox u;
+    CheckBox l;
+    CheckBox g;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptoms);
+
+        head = (LinearLayout) findViewById(R.id.HeadLayout);
+        chest = (LinearLayout) findViewById(R.id.ChestLayout);
+        uB = (LinearLayout) findViewById(R.id.UBLayout);
+        lB = (LinearLayout) findViewById(R.id.LBLayout);
+        gT = (LinearLayout) findViewById(R.id.GTLayout);
+
+        String[] headList = new String[] {};
+
+
+        final Context context = this;
+        profile = (Button) findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Home.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
